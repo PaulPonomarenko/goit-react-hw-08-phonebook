26c33794de08cab703f07f590769a575e448b07a
PaulPonomarenko <pashaponomarenkoa1999@gmail.com>
2023-07-22 10:53:57 +0300

    +-

--- src/components/FormData/FormData.jsx
+++ src/components/FormData/FormData.jsx
@@ -2,15 +2,33 @@ import React from 'react';
 import css from './FormData.module.css';
 import { useDispatch } from 'react-redux';
 import { addContact } from 'redux/contacts/operations';
+import { useState } from 'react';
 
 export function FormData() {
   const dispatch = useDispatch();
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
 
   const handleSubmit = event => {
     event.preventDefault();
-    const form = event.target;
-    dispatch(addContact({ name: form.name.value, phone: form.phone.value }));
-    form.reset();
+    dispatch(addContact({ name, phone }));
+    setName('');
+    setPhone('');
+    event.target.reset();
+  };
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'phone':
+        setPhone(value);
+        break;
+      default:
+        return;
+    }
   };
 
   return (
@@ -22,21 +40,25 @@ export function FormData() {
             <label className={css.form__label}>Name</label>
             <input
               className={css.input__change}
+              onChange={handleChange}
               type="text"
               name="name"
               required
               placeholder="Name"
+              defaultValue={name}
             />
           </div>
           <div className={css.input__form}>
             <label className={css.form__label}>Number</label>
             <input
               className={css.input__change}
+              onChange={handleChange}
               type="tel"
               name="phone"
               title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
               required
               placeholder="Number"
+              defaultValue={phone}
             />
           </div>
           <div className={css.form__button}>
